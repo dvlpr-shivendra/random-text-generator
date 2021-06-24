e7583d751328080e816cb8211c8deab98ced5ae1
Shivendra Techster <shivendratechster@gmail.com>
2021-06-24 09:10:42 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import LoremIpsumGenerator from './components/LoremIpsumGenerator.vue'
+
+new Vue({
+    el: '#app',
+    components: {
+        LoremIpsumGenerator
+    }
+})
---
+++ src/components/LoremIpsumGenerator.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Genrate lorem ipsum text</h1>
+    <button @click="generate">Generate</button>
+    <textarea v-model="text" cols="30" rows="10"></textarea>
+  </div>
+</template>
+
+<script>
+import { LoremIpsum } from "lorem-ipsum";
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+
+  methods: {
+    generate() {
+      this.text = lorem.generateParagraphs(2);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
