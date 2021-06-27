7ad3ab2202c087ecbf6448394e5c956447e3fbb9
Shivendra Techster <shivendratechster@gmail.com>
2021-06-27 22:34:30 +0530

    production assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,12 @@
 let mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 mix.js('src/app.js', 'dist').setPublicPath('dist').vue({ version: 2 });
 
-mix.sass('src/app.scss', 'dist').setPublicPath('dist');
+mix.sass('src/app.scss', 'dist')
+    .purgeCss({
+        content: [
+            "dist/**/*.html",
+            "src/components/**/*.vue",
+        ]
+    })
